7874bdb1fdeea49d1e3a00f283c97d5b1af12fe0
Fabian Joswig <fabian.joswig@ed.ac.uk>
2021-10-26 10:41:36 +0100

    benchmarks simplified

--- tests/benchmark_test.py
+++ tests/benchmark_test.py
@@ -5,42 +5,37 @@ import time
 
 np.random.seed(0)
 
-def test_bench_mul(benchmark):
-    length = 1000
-    my_obs = pe.Obs([np.random.rand(length)], ['t1'])
+length = 1000
 
-    def mul(x, y):
-        return x * y
+def mul(x, y):
+    return x * y
+
+
+def test_b_mul(benchmark):
+    my_obs = pe.Obs([np.random.rand(length)], ['t1'])
 
     benchmark(mul, my_obs, my_obs)
 
 
-def test_bench_cmul(benchmark):
-    length = 1000
+def test_b_cmul(benchmark):
     my_obs = pe.CObs(pe.Obs([np.random.rand(length)], ['t1']),
                      pe.Obs([np.random.rand(length)], ['t1']))
 
-    def mul(x, y):
-        return x * y
-
     benchmark(mul, my_obs, my_obs)
 
 
-def test_bench_matmul(benchmark):
+def test_b_matmul(benchmark):
     dim = 4
     my_list = []
-    length = 1000
     for i in range(dim ** 2):
         my_list.append(pe.Obs([np.random.rand(length)], ['t1']))
     my_array = np.array(my_list).reshape((dim, dim))
 
     benchmark(pe.linalg.matmul, my_array, my_array)
 
-
-def test_bench_cmatmul(benchmark):
+def test_b_cmatmul(benchmark):
     dim = 4
     my_list = []
-    length = 1000
     for i in range(dim ** 2):
         my_list.append(pe.CObs(pe.Obs([np.random.rand(length)], ['t1']),
                                pe.Obs([np.random.rand(length)], ['t1'])))
@@ -49,7 +44,6 @@ def test_bench_cmatmul(benchmark):
     benchmark(pe.linalg.matmul, my_array, my_array)
 
 
-def test_bench_gamma_method(benchmark):
-    length = 1000
+def test_b_gamma(benchmark):
     my_obs = pe.Obs([np.random.rand(length)], ['t1'])
     benchmark(my_obs.gamma_method)

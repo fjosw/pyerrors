c41ab8d299561b729f2307e0ab54fc8636f4d18b
Fabian Joswig <fabian.joswig@ed.ac.uk>
2021-11-07 21:03:08 +0000

    README updated

--- pyerrors/__init__.py
+++ pyerrors/__init__.py
@@ -1,6 +1,12 @@
 r'''
 # What is pyerrors?
 `pyerrors` is a python package for error computation and propagation of Markov chain Monte Carlo data.
+It is based on the **gamma method** [arXiv:hep-lat/0306017](https://arxiv.org/abs/hep-lat/0306017). Some of its features are:
+- **automatic differentiation** as suggested in [arXiv:1809.01289](https://arxiv.org/abs/1809.01289) (partly based on the [autograd](https://github.com/HIPS/autograd) package)
+- **treatment of slow modes** in the simulation as suggested in [arXiv:1009.5228](https://arxiv.org/abs/1009.5228)
+- coherent **error propagation** for data from **different Markov chains**
+- **non-linear fits with x- and y-errors** and exact linear error propagation based on automatic differentiation as introduced in [arXiv:1809.01289]
+- **real and complex matrix operations** and their error propagation based on automatic differentiation (cholesky decomposition, calculation of eigenvalues and eigenvectors, singular value decomposition...)
 
 ## Getting started
 

ec4bb393990199f75e950a03624097c1198c9631
Fabian Joswig <fabian.joswig@ed.ac.uk>
2021-12-07 17:11:50 +0000

    test: test for dirac gamma added

--- tests/dirac_test.py
+++ tests/dirac_test.py
@@ -10,3 +10,23 @@ def test_gamma_matrices():
         assert np.allclose(matrix @ matrix, np.identity(4))
         assert np.allclose(matrix, matrix.T.conj())
     assert np.allclose(pe.dirac.gamma5, pe.dirac.gamma[0] @ pe.dirac.gamma[1] @ pe.dirac.gamma[2] @ pe.dirac.gamma[3])
+
+
+def test_grid_dirac():
+    for gamma in ['Identity',
+                  'Gamma5',
+                  'GammaX',
+                  'GammaY',
+                  'GammaZ',
+                  'GammaT',
+                  'GammaXGamma5',
+                  'GammaYGamma5',
+                  'GammaZGamma5',
+                  'GammaTGamma5',
+                  'SigmaXT',
+                  'SigmaXY',
+                  'SigmaXZ',
+                  'SigmaYT',
+                  'SigmaYZ',
+                  'SigmaZT']:
+        pe.dirac.Grid_gamma(gamma)

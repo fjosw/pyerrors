5ebfda5250573050a5797e58886b0a1d8faf5b91
Fabian Joswig <fabian.joswig@ed.ac.uk>
2021-12-10 14:20:51 +0000

    test: test for linalg.eig added

--- tests/linalg_test.py
+++ tests/linalg_test.py
@@ -300,6 +300,10 @@ def test_matrix_functions():
         for j in range(dim):
             assert tmp[j].is_zero()
 
+    # Check eig function
+    e2 = pe.linalg.eig(sym)
+    assert np.all(e == e2[::-1])
+
     # Check svd
     u, v, vh = pe.linalg.svd(sym)
     diff = sym - u @ np.diag(v) @ vh

64a8bc690fe2d34ffecfba222d65429e51982ee8
Fabian Joswig <fabian.joswig@ed.ac.uk>
2021-12-23 12:21:40 +0100

    refactor!: Obs.print method removed

--- pyerrors/obs.py
+++ pyerrors/obs.py
@@ -434,10 +434,6 @@ class Obs:
                 my_string_list.append(my_string)
             print('\n'.join(my_string_list))
 
-    def print(self, level=1):
-        warnings.warn("Method 'print' renamed to 'details'", DeprecationWarning)
-        self.details(level > 1)
-
     def is_zero_within_error(self, sigma=1):
         """Checks whether the observable is zero within 'sigma' standard errors.
 

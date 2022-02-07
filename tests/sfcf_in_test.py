0d1921c16b221e28b844cd9cb8c4b00b308bb4f2
jkuhl-uni <j_kuhl19@uni-muenster.de>
2022-02-07 12:28:14 +0100

    positive tests for o and c finished

--- tests/sfcf_in_test.py
+++ tests/sfcf_in_test.py
@@ -11,19 +11,68 @@ import pytest
 
 from time import sleep
 
-n = 5
-def o_test():
-    for i in range(2,n+1):
-        os.mkdir("data/sfcf_test/data_o/cfg"+str(i))
-        shutil.copy("data/sfcf_test/data_o/cfg1/f_1","data/sfcf_test/data_o/cfg"+str(i))
-        shutil.copy("data/sfcf_test/data_o/cfg1/f_A","data/sfcf_test/data_o/cfg"+str(i))
-        shutil.copy("data/sfcf_test/data_o/cfg1/F_V0","data/sfcf_test/data_o/cfg"+str(i))
+def build_test_environment(type, cfgs, reps):
+    if type == "o":
+        for i in range(2,cfgs+1):
+            shutil.copytree("data/sfcf_test/data_o/test_r0/cfg1","data/sfcf_test/data_o/test_r0/cfg"+str(i))
+        for i in range(1,reps):
+            shutil.copytree("data/sfcf_test/data_o/test_r0", "data/sfcf_test/data_o/test_r"+str(i))
+    elif type == "c":
+        for i in range(2,cfgs+1):
+            shutil.copy("data/sfcf_test/data_c/data_c_r0/data_c_r0_n1","data/sfcf_test/data_c/data_c_r0/data_c_r0_n"+str(i))
+        for i in range(1,reps):
+            os.mkdir("data/sfcf_test/data_c/data_c_r"+str(i))
+            for j in range(1,cfgs+1):
+                shutil.copy("data/sfcf_test/data_c/data_c_r0/data_c_r0_n1","data/sfcf_test/data_c/data_c_r"+str(i)+"/data_c_r"+str(i)+"_n"+str(j))
 
-    o = sfin.read_sfcf("data/sfcf_test/data_o", "qcd2sf_T24L24_b3.685_k0.1394400_id0", "f_A",quarks="lquark lquark", noffset=15)#, files = ["qcd2sf_T24L24_b3.685_k0.1394400_id0_r0_n50","qcd2sf_T24L24_b3.685_k0.1394400_id0_r0_n100","qcd2sf_T24L24_b3.685_k0.1394400_id0_r0_n120","qcd2sf_T24L24_b3.685_k0.1394400_id0_r0_n140","qcd2sf_T24L24_b3.685_k0.1394400_id0_r0_n150"])
 
 
-    sleep(10)
-    for i in range(2,n+1):
-        shutil.rmtree("data/sfcf_test/data_o/cfg"+str(i))
+
+def clean_test_environment(type, cfgs, reps):
+    if type == "o":
+        for i in range(1,reps):
+            shutil.rmtree("data/sfcf_test/data_o/test_r"+str(i))
+        for i in range(2,cfgs+1):
+            shutil.rmtree("data/sfcf_test/data_o/test_r0/cfg"+str(i))
+    elif type == "c":
+        for i in range(1,reps):
+            shutil.rmtree("data/sfcf_test/data_c/data_c_r"+str(i))
+        for i in range(2,cfgs+1):
+            os.remove("data/sfcf_test/data_c/data_c_r0/data_c_r0_n"+str(i))
+
         
-    
+def o_bb_test():
+    build_test_environment("o",5,3)
+    f_1 = sfin.read_sfcf("data/sfcf_test/data_o", "test", "f_1",quarks="lquark lquark", wf = 0, wf2=0, version = "2.0", single = True)
+    print(f_1)
+    clean_test_environment("o",5,3)
+        
+def o_bi_test():
+    build_test_environment("o",5,3)
+    f_A = sfin.read_sfcf("data/sfcf_test/data_o", "test", "f_A",quarks="lquark lquark", wf = 0, version = "2.0")
+    print(f_A)
+    clean_test_environment("o",5,3)
+            
+def o_bib_test():
+    build_test_environment("o",5,3)
+    f_V0 = sfin.read_sfcf("data/sfcf_test/data_o", "test", "F_V0",quarks="lquark lquark", wf = 0, wf2 = 0, version = "2.0", b2b = True)
+    print(f_V0)
+    clean_test_environment("o",5,3)
+
+def c_bb_test():
+    build_test_environment("c",5,3)
+    f_1 = sfin.read_sfcf("data/sfcf_test/data_c", "data_c", "f_1", quarks="lquark lquark", wf = 0, wf2=0, version = "2.0c", single = True)
+    print(f_1)
+    clean_test_environment("c",5,3)
+
+def c_bi_test():
+    build_test_environment("c",5,3)
+    f_1 = sfin.read_sfcf("data/sfcf_test/data_c", "data_c", "f_A", quarks="lquark lquark", wf = 0, version = "2.0c")
+    print(f_1)
+    clean_test_environment("c",5,3)
+
+def c_bib_test():
+    build_test_environment("c",5,3)
+    f_V0 = sfin.read_sfcf("data/sfcf_test/data_c", "data_c", "F_V0",quarks="lquark lquark", wf = 0, wf2 = 0, version = "2.0c", b2b = True)
+    print(f_V0)
+    clean_test_environment("c",5,3)

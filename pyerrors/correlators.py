5da16c29ffb0d70c291d2679e51ea70edab48c40
JanNeuendorf <jp333n@gmx.de>
2022-02-17 18:07:25 +0100

    repair tests 2

--- pyerrors/correlators.py
+++ pyerrors/correlators.py
@@ -71,7 +71,7 @@ class Corr:
         if isinstance(data_input, list):
 
             if all([isinstance(item, (Obs, CObs)) for item in data_input]):
-                _assert_equal_properties(data_input,allow_missing=True)
+                _assert_equal_properties(data_input, allow_missing=True)
                 self.content = [np.asarray([item]) for item in data_input]
             if all([isinstance(item, (Obs, CObs)) or item is None for item in data_input]):
                 _assert_equal_properties([o for o in data_input if o is not None], allow_missing=True)

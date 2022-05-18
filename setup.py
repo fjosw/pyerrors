5bd10cb81dc2c452bd2fd98aaffe33bc264c5818
Fabian Joswig <fabian.joswig@ed.ac.uk>
2022-05-18 12:15:28 +0100

    docs: long_description added to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python
-
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(name='pyerrors',
       version='2.1.0+dev',
       description='Error analysis for lattice QCD',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Fabian Joswig',
       author_email='fabian.joswig@ed.ac.uk',
       packages=find_packages(),

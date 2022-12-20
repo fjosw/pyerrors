c01d822ec8a1e7c3cd7182649df17e884f6340a9
Fabian Joswig <fabian.joswig@ed.ac.uk>
2022-12-20 17:09:35 +0100

    refactor: unnecessary if clause in constructor of Corr class removed.

--- pyerrors/correlators.py
+++ pyerrors/correlators.py
@@ -69,9 +69,6 @@ class Corr:
 
         if isinstance(data_input, list):
 
-            if all([isinstance(item, (Obs, CObs)) for item in data_input]):
-                _assert_equal_properties(data_input)
-                self.content = [np.asarray([item]) for item in data_input]
             if all([isinstance(item, (Obs, CObs)) or item is None for item in data_input]):
                 _assert_equal_properties([o for o in data_input if o is not None])
                 self.content = [np.asarray([item]) if item is not None else None for item in data_input]

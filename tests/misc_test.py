c03e4669a0d8a4a44da6431371156bb77042f41b
Fabian Joswig <fabian.joswig@ed.ac.uk>
2023-01-18 17:01:37 +0000

    tests: additional tests for generalization of misc.errorbar added.

--- tests/misc_test.py
+++ tests/misc_test.py
@@ -12,6 +12,9 @@ def test_obs_errorbar():
         x_obs.append(pe.pseudo_Obs(x, 0.1, "test"))
         y_obs.append(pe.pseudo_Obs(x ** 2, 0.1, "test"))
 
-    pe.errorbar(x_float, y_obs, marker="x", ms=2)
-    pe.errorbar(x_obs, y_obs, marker="x", ms=2)
+    for xerr in [2, None]:
+        for yerr in [0.1, None]:
+            pe.errorbar(x_float, y_obs, marker="x", ms=2, xerr=xerr, yerr=yerr)
+            pe.errorbar(x_obs, y_obs, marker="x", ms=2, xerr=xerr, yerr=yerr)
 
+    plt.close('all')

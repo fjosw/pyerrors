8ed5ce9569e019ce3df55e4f8c5a0a24b8979893
Fabian Joswig <fabian.joswig@ed.ac.uk>
2023-03-11 21:17:09 +0000

    feat: print_config function added to help with debugging.

--- pyerrors/version.py
+++ pyerrors/version.py
@@ -1,1 +1,21 @@
-__version__ = "2.7.0+dev"
+import platform
+import numpy as np
+import scipy
+import matplotlib
+import pandas as pd
+
+
+__version__ = "2.7.0-dev"
+
+
+def print_config():
+    """Print information about version of python, pyerrors and dependencies."""
+    config = {"python": platform.python_version(),
+              "pyerrors": __version__,
+              "numpy": np.__version__,
+              "scipy": scipy.__version__,
+              "matplotlib": matplotlib.__version__,
+              "pandas": pd.__version__}
+
+    for key, value in config.items():
+        print(f"{key : <10}\t {value}")

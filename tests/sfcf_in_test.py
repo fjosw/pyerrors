912e0bfee985a73b3835644a93ec366c65102ec3
Justus <j_kuhl19@uni-muenster.de>
2023-03-15 16:23:34 +0000

    use tmp_path fixture

--- tests/sfcf_in_test.py
+++ tests/sfcf_in_test.py
@@ -11,120 +11,95 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
 
-def build_test_environment(env_type, cfgs, reps):
+def build_test_environment(path, env_type, cfgs, reps):
+    shutil.copytree("tests/data/sfcf_test/data_"+env_type, (path + "/data_" + env_type))
     if env_type == "o":
         for i in range(2,cfgs+1):
-            shutil.copytree("tests/data/sfcf_test/data_o/test_r0/cfg1","tests/data/sfcf_test/data_o/test_r0/cfg"+str(i))
+            shutil.copytree(path + "/data_o/test_r0/cfg1", path + "/data_o/test_r0/cfg"+str(i))
         for i in range(1,reps):
-            shutil.copytree("tests/data/sfcf_test/data_o/test_r0", "tests/data/sfcf_test/data_o/test_r"+str(i))
+            shutil.copytree(path + "/data_o/test_r0", path + "/data_o/test_r"+str(i))
     elif env_type == "c":
         for i in range(2,cfgs+1):
-            shutil.copy("tests/data/sfcf_test/data_c/data_c_r0/data_c_r0_n1","tests/data/sfcf_test/data_c/data_c_r0/data_c_r0_n"+str(i))
+            shutil.copy(path + "/data_c/data_c_r0/data_c_r0_n1", path + "/data_c/data_c_r0/data_c_r0_n"+str(i))
         for i in range(1,reps):
-            os.mkdir("tests/data/sfcf_test/data_c/data_c_r"+str(i))
+            os.mkdir(path + "/data_c/data_c_r"+str(i))
             for j in range(1,cfgs+1):
-                shutil.copy("tests/data/sfcf_test/data_c/data_c_r0/data_c_r0_n1","tests/data/sfcf_test/data_c/data_c_r"+str(i)+"/data_c_r"+str(i)+"_n"+str(j))
+                shutil.copy(path + "/data_c/data_c_r0/data_c_r0_n1",path + "/data_c/data_c_r"+str(i)+"/data_c_r"+str(i)+"_n"+str(j))
 
 
-
-def clean_test_environment(env_type, cfgs, reps):
-    if env_type == "o":
-        for i in range(1,reps):
-            shutil.rmtree("tests/data/sfcf_test/data_o/test_r"+str(i))
-        for i in range(2,cfgs+1):
-            shutil.rmtree("tests/data/sfcf_test/data_o/test_r0/cfg"+str(i))
-    elif env_type == "c":
-        for i in range(1,reps):
-            shutil.rmtree("tests/data/sfcf_test/data_c/data_c_r"+str(i))
-        for i in range(2,cfgs+1):
-            os.remove("tests/data/sfcf_test/data_c/data_c_r0/data_c_r0_n"+str(i))
-
-
-def test_o_bb():
-    build_test_environment("o",5,3)
-    f_1 = sfin.read_sfcf("tests/data/sfcf_test/data_o", "test", "f_1",quarks="lquark lquark", wf = 0, wf2=0, version = "2.0", corr_type="bb")
+def test_o_bb(tmp_path):
+    build_test_environment(str(tmp_path), "o",5,3)
+    f_1 = sfin.read_sfcf(str(tmp_path) + "/data_o", "test", "f_1",quarks="lquark lquark", wf = 0, wf2=0, version = "2.0", corr_type="bb")
     print(f_1)
-    clean_test_environment("o",5,3)
     assert len(f_1) == 1
     assert f_1[0].value == 351.1941525454502
 
-def test_o_bi():
-    build_test_environment("o",5,3)
-    f_A = sfin.read_sfcf("tests/data/sfcf_test/data_o", "test", "f_A",quarks="lquark lquark", wf = 0, version = "2.0")
+def test_o_bi(tmp_path):
+    build_test_environment(str(tmp_path), "o",5,3)
+    f_A = sfin.read_sfcf(str(tmp_path) + "/data_o", "test", "f_A",quarks="lquark lquark", wf = 0, version = "2.0")
     print(f_A)
-    clean_test_environment("o",5,3)
     assert len(f_A) == 3
     assert f_A[0].value == 65.4711887279723
     assert f_A[1].value == 1.0447210336915187
     assert f_A[2].value == -41.025094911185185
 
-def test_o_bib():
-    build_test_environment("o",5,3)
-    f_V0 = sfin.read_sfcf("tests/data/sfcf_test/data_o", "test", "F_V0",quarks="lquark lquark", wf = 0, wf2 = 0, version = "2.0", corr_type="bib")
+def test_o_bib(tmp_path):
+    build_test_environment(str(tmp_path), "o",5,3)
+    f_V0 = sfin.read_sfcf(str(tmp_path) + "/data_o", "test", "F_V0",quarks="lquark lquark", wf = 0, wf2 = 0, version = "2.0", corr_type="bib")
     print(f_V0)
-    clean_test_environment("o",5,3)
     assert len(f_V0) == 3
     assert f_V0[0] == 683.6776090085115
     assert f_V0[1] == 661.3188585582334
     assert f_V0[2] == 683.6776090081005
 
-def test_c_bb():
-    build_test_environment("c",5,3)
-    f_1 = sfin.read_sfcf("tests/data/sfcf_test/data_c", "data_c", "f_1", quarks="lquark lquark", wf = 0, wf2=0, version = "2.0c", corr_type="bb")
+def test_c_bb(tmp_path):
+    build_test_environment(str(tmp_path), "c",5,3)
+    f_1 = sfin.read_sfcf(str(tmp_path) + "/data_c", "data_c", "f_1", quarks="lquark lquark", wf = 0, wf2=0, version = "2.0c", corr_type="bb")
     print(f_1)
-    clean_test_environment("c",5,3)
     assert len(f_1) == 1
     assert f_1[0].value == 351.1941525454502
 
-def test_c_bi():
-    build_test_environment("c",5,3)
-    f_A = sfin.read_sfcf("tests/data/sfcf_test/data_c", "data_c", "f_A", quarks="lquark lquark", wf = 0, version = "2.0c")
+def test_c_bi(tmp_path):
+    build_test_environment(str(tmp_path), "c",5,3)
+    f_A = sfin.read_sfcf(str(tmp_path) + "/data_c", "data_c", "f_A", quarks="lquark lquark", wf = 0, version = "2.0c")
     print(f_A)
-    clean_test_environment("c",5,3)
     assert len(f_A) == 3
     assert f_A[0].value == 65.4711887279723
     assert f_A[1].value == 1.0447210336915187
     assert f_A[2].value == -41.025094911185185
 
-def test_c_bib():
-    build_test_environment("c",5,3)
-    f_V0 = sfin.read_sfcf("tests/data/sfcf_test/data_c", "data_c", "F_V0",quarks="lquark lquark", wf = 0, wf2 = 0, version = "2.0c", corr_type="bib")
+def test_c_bib(tmp_path):
+    build_test_environment(str(tmp_path), "c",5,3)
+    f_V0 = sfin.read_sfcf(str(tmp_path) + "/data_c", "data_c", "F_V0",quarks="lquark lquark", wf = 0, wf2 = 0, version = "2.0c", corr_type="bib")
     print(f_V0)
-    clean_test_environment("c",5,3)
     assert len(f_V0) == 3
     assert f_V0[0] == 683.6776090085115
     assert f_V0[1] == 661.3188585582334
     assert f_V0[2] == 683.6776090081005
 
-def test_a_bb():
-    build_test_environment("a",5,3)
+def test_a_bb(tmp_path):
     f_1 = sfin.read_sfcf("tests/data/sfcf_test/data_a", "data_a", "f_1", quarks="lquark lquark", wf = 0, wf2=0, version = "2.0a", corr_type="bb")
     print(f_1)
-    clean_test_environment("a",5,3)
     assert len(f_1) == 1
     assert f_1[0].value == 351.1941525454502
 
-def test_a_bi():
-    build_test_environment("a",5,3)
+def test_a_bi(tmp_path):
     f_A = sfin.read_sfcf("tests/data/sfcf_test/data_a", "data_a", "f_A", quarks="lquark lquark", wf = 0, version = "2.0a")
     print(f_A)
-    clean_test_environment("a",5,3)
     assert len(f_A) == 3
     assert f_A[0].value == 65.4711887279723
     assert f_A[1].value == 1.0447210336915187
     assert f_A[2].value == -41.02509491118518
 
-def test_a_bib():
-    build_test_environment("a",5,3)
+def test_a_bib(tmp_path):
     f_V0 = sfin.read_sfcf("tests/data/sfcf_test/data_a", "data_a", "F_V0",quarks="lquark lquark", wf = 0, wf2 = 0, version = "2.0a", corr_type="bib")
     print(f_V0)
-    clean_test_environment("a",5,3)
     assert len(f_V0) == 3
     assert f_V0[0] == 683.6776090085115
     assert f_V0[1] == 661.3188585582334
     assert f_V0[2] == 683.6776090081005
 
-def test_find_corr():
+def test_find_corr(tmp_path):
     pattern = 'name      ' + "f_A" + '\nquarks    ' + "lquark lquark" + '\noffset    ' + str(0) + '\nwf        ' + str(0)
     start_read, T = sfin._find_correlator("tests/data/sfcf_test/data_c/data_c_r0/data_c_r0_n1", "2.0c", pattern, False)
     assert start_read == 21
@@ -138,7 +113,7 @@ def test_find_corr():
     with pytest.raises(Exception):
         sfin._find_correlator("tests/data/sfcf_test/broken_data_c/data_c_r0/data_c_r0_n1", "2.0c", pattern, False)
     
-def test_read_compact_file():
+def test_read_compact_file(tmp_path):
     rep_path = "tests/data/sfcf_test/broken_data_c/data_c_r0/"
     config_file = "data_c_r0_n1"
     start_read = 469

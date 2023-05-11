67c8e1e919710373dae7429a4593c3146dea2b23
Justus Kuhlmann <j_kuhl19@uni-muenster.de>
2023-05-11 14:35:41 +0000

    revert changes

--- pyerrors/input/json.py
+++ pyerrors/input/json.py
@@ -162,8 +162,6 @@ def create_json_string(ol, description='', indent=1):
             dat['tag']['prange'] = my_corr.prange
         return dat
 
-    if ol is None:
-        return None
     if not isinstance(ol, list):
         ol = [ol]
 
@@ -481,10 +479,7 @@ def import_json_string(json_string, verbose=True, full_output=False):
     result : dict
         if full_output=True
     """
-    if json_string is None:
-        return None
-    else:
-        return _parse_json_dict(json.loads(json_string), verbose, full_output)
+    return _parse_json_dict(json.loads(json_string), verbose, full_output)
 
 
 def load_json(fname, verbose=True, gz=True, full_output=False):

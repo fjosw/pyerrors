649914d13d91cc5ba8dc6c4c623dba2f03e70db1
Fabian Joswig <fabian.joswig@ed.ac.uk>
2023-07-14 14:03:57 +0100

    tests: two trivial tests for integration added.

--- tests/integrate_test.py
+++ tests/integrate_test.py
@@ -39,3 +39,9 @@ def test_integration():
 
     check_ana_vs_int(pobs, lobs, uobs, epsabs=1.e-9, epsrel=1.236e-10, limit=100)
     assert(len(pe.integrate.quad(f, pobs, lobs, uobs, full_output=True)) > 2)
+
+    r1, _ = pe.integrate.quad(F, pobs, 1, 0.1)
+    r2, _ = pe.integrate.quad(F, pobs, 0.1, 1)
+    assert r1 == -r2
+    iamzero, _ = pe.integrate.quad(F, pobs, 1, 1)
+    assert iamzero == 0

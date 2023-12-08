e07de53fee7fb38427e11e8b72c307cc5e546add
Fabian Joswig <fabian.joswig@uni-muenster.de>
2023-12-08 19:51:43 +0100

    [tests] Check kn special function derivative explicitly vs numerical derivative of scipy function.

--- tests/special_test.py
+++ tests/special_test.py
@@ -9,4 +9,4 @@ from numdifftools import Jacobian as num_jacobian
 def test_kn():
     for n in np.arange(0, 10):
         for val in np.linspace(0.1, 7.3, 10):
-            assert np.isclose(num_jacobian(lambda x: pe.special.kn(n, x))(val), jacobian(lambda x: pe.special.kn(n, x))(val), rtol=1e-10, atol=1e-10)
+            assert np.isclose(num_jacobian(lambda x: scipy.special.kn(n, x))(val), jacobian(lambda x: pe.special.kn(n, x))(val), rtol=1e-10, atol=1e-10)

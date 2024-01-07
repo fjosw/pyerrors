871dfed3093c3c1e4a765ca803fab8279afb5e3b
Fabian Joswig <fabian.joswig@uni-muenster.de>
2024-01-07 17:11:02 +0100

    [feat] Imports corrected, docstring added.

--- pyerrors/special.py
+++ pyerrors/special.py
@@ -1,17 +1,20 @@
 import scipy
 import numpy as np
 from autograd.extend import primitive, defvjp
-from autograd.scipy.special import j0, y0, j1, y1, jn, yn, i0, i1, iv, ive
+from autograd.scipy.special import j0, y0, j1, y1, jn, yn, i0, i1, iv, ive, beta, betainc, betaln
+from autograd.scipy.special import polygamma, psi, digamma, gamma, gammaln, gammainc, gammaincc, gammasgn, rgamma, multigammaln
+from autograd.scipy.special import erf, erfc, erfinv, erfcinv, logit, expit, logsumexp
 
 
 __all__ = ["beta", "betainc", "betaln",
            "polygamma", "psi", "digamma", "gamma", "gammaln", "gammainc", "gammaincc", "gammasgn", "rgamma", "multigammaln",
            "kn", "j0", "y0", "j1", "y1", "jn", "yn", "i0", "i1", "iv", "ive",
-           "erf", "erfc", "erfinc", "erfcinv", "logit", "expit", "logsumsexp"]
+           "erf", "erfc", "erfinv", "erfcinv", "logit", "expit", "logsumexp"]
 
 
 @primitive
 def kn(n, x):
+    """Modified Bessel function of the second kind of integer order n"""
     if int(n) != n:
         raise TypeError("The order 'n' needs to be an integer.")
     return scipy.special.kn(n, x)
